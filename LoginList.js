d95061c7eca3508eeee8034df79e76f090acc4c7
Andrew Monshizadeh <amonshiz@gmail.com>
2014-10-21 17:52:25 -0500

    refactored to dry up clicks

--- LoginList.js
+++ LoginList.js
@@ -1,34 +1,44 @@
 safari.application.addEventListener("popover", popoverHandler, true);
 const theGlobal = safari.extension.globalPage.contentWindow.amonshizGlobal;
 
-function popoverHandler () {
-  buildLoginsList();
+function popoverHandler() {
+  showLoginsList();
 }
 
-function buildLoginsList () {
+function buildLoginsList() {
   var groupDivs = _.map(theGlobal.getGroups(), function (curGroup, curGroupIndex) {
     var loginsTable = $('<table></table>', {
-      class : "table"
+      class: "table"
     });
     _.forEach(curGroup["logins"], function (curLogin, curLoginIndex) {
       var loginRow = $('<tr></tr>').append($('<td></td>', {
-        text : curLogin["userName"]
-      })).append($('<td></td>').append($('<a></a>', {
-          text : 'Login',
-          href : '#',
-          click : function (event) {
+          text: curLogin["userName"]
+        })).append($('<td></td>').append($('<a></a>', {
+          text: 'Login',
+          class: 'btn btn-primary btn-sm',
+          href: '#',
+          click: function (event) {
             event.preventDefault();
             safari.application.activeBrowserWindow.openTab().url = 'https://' + theGlobal.orgTypes[curLogin.type] + '.salesforce.com/?un=' + curLogin.userName + '&pw=' + curLogin.password;
           }
-      })));
+        }))).append($('<td></td>').append($('<a></a>', {
+          text: 'Delete',
+          class: 'btn btn-default btn-sm',
+          href: '#',
+          click: function (evvent) {
+            event.preventDefault();
+            theGlobal.deleteAccount(curLogin);
+            showLoginsList();
+          }
+        })));
       loginsTable.append(loginRow);
     });
     return $('<div></div>', {
-      class : "panel panel-default"
+      class: "panel panel-default"
     }).append($('<div></div>', {
-      class : "panel-heading",
-      text : curGroup["groupName"]
-    }).add(loginsTable));
+        class: "panel-heading",
+        text: curGroup["groupName"]
+      }).add(loginsTable));
   });
 
   var loginsList = $('#loginsList');
@@ -38,15 +48,39 @@ function buildLoginsList () {
   });
 }
 
-$(document).ready( function () {
-  $('#addLogin').click( function (event) {
+function saveNewLogin() {
+  var account = {};
+  account['userName'] = $('#newLoginUsername').val();
+  account['password'] = $('#newLoginPassword').val();
+  account['type'] = $("input[name=\"org-type\"]:checked").val();
+  account['description'] = 'Just some description text because.';
+  var groupName = $('#newLoginGroup').val();
+  theGlobal.addAccount(account, (groupName === undefined || groupName === '') ? 'Uncategorized' : groupName);
+}
+
+function showLoginsList() {
+  buildLoginsList();
+  $('#loginsListWrapper').show();
+  $('#newLoginWrapper').hide();
+}
+
+function showNewLogin() {
+  $('#loginsListWrapper').hide();
+  $('#newLoginWrapper').show();
+}
+
+$(document).ready(function () {
+  $('#addLogin').click(function (event) {
     event.preventDefault();
-    $('#loginsListWrapper').hide();
-    $('#newLoginWrapper').show();
+    showNewLogin();
   });
-  $('#saveLogin').click( function (event) {
+  $('#saveLogin').click(function (event) {
     event.preventDefault();
-    $('#loginsListWrapper').show();
-    $('#newLoginWrapper').hide();
+    saveNewLogin();
+    $('#cancelLogin').click();
   });
+  $('#cancelLogin').click(function (event) {
+    event.preventDefault();
+    showLoginsList();
+  })
 })
